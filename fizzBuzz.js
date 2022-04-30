01ed4aff56b7e870af34b23c1f4270eda0f12f20
Harshitha-Killi <93632640+Harshitha-Killi@users.noreply.github.com>
2022-04-30 22:11:16 +0530

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,25 +1,27 @@
 var output=[];
 var count=1;
 
-function fizzbuzz(){
+function fizzBuzz(){
+  
+  if(count%3===0 && count%15!==0){
+     output.push("Fizz");
+  }
 
+   else if(count%5===0 && count%15!==0){
+     output.push("Buzz"); 
+  }
 
- if(count%3===0 && count%5!==0){
-    output.push("Fizz");
- }
+   else if(count%15===0){
+     output.push("FizzBuzz");
+  }
 
- else if(count%5===0 && count%3!==0){
-      output.push("Buzz");
- }
+  else{
+     output.push(count);
+  }
 
- else if(count%15===0){
-      output.push("FizzBuzz");
+  console.log(output);
+  count++;
+ 
 
- }
- else{
-      output.push(count);
- }
-
- count++;
-    console.log(output);
+  
 }
