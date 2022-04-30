240e3c3b2a6ac144d92d935b6c536372f86a46cf
Harshitha-Killi <93632640+Harshitha-Killi@users.noreply.github.com>
2022-04-30 20:17:16 +0530

    Update bmiCalculator.js

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -1,8 +1,21 @@
 // Method-1
 
 function bmiCalculator(weight,height){
-   var bmi= weight/(height*height)
-   return Math.round(bmi);
+    var bmi= weight/(height*height);
+
+    if(bmi<18.5){
+        return("Your BMI is " +bmi+ ". So, you are underweight.");
+    }
+
+    else if(bmi>18.5 && bmi<24.9){
+        return("Your BMI is " +bmi+ ". So, you have a normal weight.");
+    }
+
+    else if(bmi>24.9){
+        return("Your BMI is " +bmi+ ". So, you are overweight.");
+    }
+
 }
+
 
 // Method-2
