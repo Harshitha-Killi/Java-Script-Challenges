718b863d0e6adf4c42fb7108aeecc70d89b227b0
Harshitha-Killi <93632640+Harshitha-Killi@users.noreply.github.com>
2022-04-30 22:38:12 +0530

    Update whoIsPaying.js

--- whoIsPaying.js
+++ whoIsPaying.js
@@ -1,1 +1,14 @@
+function whoisPaying(names){
 
+  var names=["ha","hb","hc","hd"];
+
+  var randomNumber= names.length;
+  var randomPosition= (Math.floor( Math.random() * randomNumber)) ;
+  var randomName=names[randomPosition];
+
+  return randomName+" is going to pay the lunch today!" ;
+  
+}
+
+var output=whoisPaying();
+console.log(output);
